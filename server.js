3f918c90e04b279ff9ed498fde74d28be07294ea
Nahid <jr6259094@gmail.com>
2026-07-28 18:47:58 +0600

    fix server.js to remove specific port to run like app.listent4000

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRouter);
-expressServer.listen(4000, () => {
-  console.log("server is running on http://localhost:4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
