c6e7468498be180a13642c85ea6013ed52caad53
Nahid <jr6259094@gmail.com>
2026-07-29 20:36:48 +0600

    last commit befor go home

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRouter);
+
+app.get("/", (req, res) => {
+  return res.send("welcome");
+});
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
